0342cccda582c4cad3c4ed0e51d5b3784d96c94d
willowmck <willowmck@gmail.com>
2015-09-11 17:06:51 +0000

    Changed name of controller for the sake of the testing exercise

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('MasterBlaster', []);
 
-app.controller('BlastList', ['$scope', '$http', 'BlastService', function($scope, $http, BlastService) {
+app.controller('BlastController', ['$scope', '$http', 'BlastService', function($scope, $http, BlastService) {
   BlastService.list().success(function(data) {
     $scope.allBlasts = $scope.removeInactiveUsers(data.records);
   });
